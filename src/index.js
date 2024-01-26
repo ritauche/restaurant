d565577ef7ea4599b2f192553869be6c35e95765
ritauche <ritauchee@gmail.com>
2024-01-26 16:19:59 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -3,15 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import logo from './logo.svg';
+// import Table from './components/Table';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// var appname ="Devconf";
+// const banner = ReactDOM.createRoot(document.getElementById('banner'));
+
+// // let obj = {border:"2px solid orange", backgroundColor:lightblue};
+
+
+// banner.render(
+//     <React.StrictMode>
+//       {/* <h1 className='alert alert-danger'>Hello World {appname.toLowerCase()}</h1> */}
+//       <img className='img-fluid' style={{border:"3px solid red"}} src={logo} onClick={()=>{alert('hello')}}/>
+//     </React.StrictMode>
+// );
+
+// const news = ReactDOM.createRoot(document.getElementById('news'));
+
+// // news.render(
+// //   <React.StrictMode>
+// //     <Table/>
+// //     </React.StrictMode>
+// // );
+
+
+
+
+
